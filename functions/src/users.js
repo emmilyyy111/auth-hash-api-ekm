841289905a7d655fb2398bec0ff1bdb4a04bdf43
Emily Morgenbesser <emilymorgenbesser@Emilys-MacBook-Pro.local>
2021-08-17 11:51:11 -0400

    added token

--- functions/src/users.js
+++ functions/src/users.js
@@ -1,48 +1,90 @@
 const admin = require('firebase-admin')
+const jwt = require('jsonwebtoken')
 const creds = require('../credentials.json')
+const { secret } = require('../secrets')
 
 function connectDb() {
-    if(!admin.apps.length) {
-        admin.initializeApp({
-            credential: admin.credential.cert(creds)
-        })
-    }
-    return admin.firestore()
+  if (!admin.apps.length) {
+    admin.initializeApp({
+      credential: admin.credential.cert(creds),
+    })
+  }
+  return admin.firestore()
 }
 
 exports.userSignup = (req, res) => {
-    if(!req.body || !req.body.email || !req.body.password) {
-        res.status(400).send({
-            message: 'Invalid Request: missing email or password',
-            status: 400,
-            success: false
-        })
-        return
-    }
+  if (!req.body || !req.body.email || !req.body.password) {
+    res.status(400).send({
+      message: 'Invalid Request: missing email or password',
+      status: 400,
+      success: false,
+    })
+    return
+  }
 
-const db = connectDb()
+  const db = connectDb()
 
-db.collection('users')
-.doc(req.body.email.toLowerCase())
-.set(req.body)
-.then((res) => {
-    res.send({
+  db.collection('users')
+    .doc(req.body.email.toLowerCase())
+    .set(req.body)
+    .then(() => {
+        const token = jwt.sign({ email: req.body.email }, secret)
+      res.send({
         message: 'connection successful',
         status: 200,
-        success: true
+        success: true,
+        token
+      })
     })
-})
-.catch(err => {
-    res.status(500).send({
+    .catch(err => {
+      res.status(500).send({
         message: 'Error:' + err.message,
         status: 500,
-        success: false
+        success: false,
+      })
     })
-})
-
 }
 
-
 exports.userLogin = (req, res) => {
-    res.status(200).send("logged in")
+  if (!req.body || !req.body || !req.body.password) {
+    res.status(400).send({
+      message: 'Invalid Request: missing email or password',
+      status: 400,
+      success: false,
+    })
+    return
+  }
+  const db = connectDb()
+  db.collection('users')
+    .where('email', '==', req.body.email.toLowerCase())
+    .where('password', '==', req.body.password)
+    .get()
+    .then(userCollection => {
+      if (userCollection.docs.length) {
+        let user = userCollection.docs[0].data()
+        user.password = undefined
+
+        const token = jwt.sign(user, secret)
+        res.send({
+          message: 'User logged in successfully',
+          status: 200,
+          success: true,
+          token
+        })
+      }
+       else {
+        res.status(401).send({
+          message: 'Invalid email or password',
+          status: 401,
+          success: false,
+        })
+      }
+    })
+    .catch(err => {
+      res.status(500).send({
+        message: 'Error:' + err.message,
+        status: 500,
+        success: false,
+      })
+    })
 }
